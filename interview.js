a1cb66fe0d562e191e78cf8452c48030a083bdde
Anurag <anuragraveendren98@gmail.com>
2024-11-16 01:12:58 +0530

    updated

---
+++ interview.js
@@ -0,0 +1,12 @@
+const vowels=["a","e","i","o","u"]
+
+const count=((value)=>{
+
+  const count =0
+return value.filter((data)=>vowels.includes(data)).length
+
+})
+console.log(count(["a","b","c","d","e","f"]));
+
+
+// -------------------------------------------------------------------   
